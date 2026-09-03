12cd4c2da6fc0986e7ceb6f6164ba6e356843f79
Sieghartm4 <sieghartm4@gmail.com>
2026-09-03 11:14:18 +0800

    [UPDATE]

--- server/src/database/seeders/20260902000001-tax_compliance_route_access.js
+++ server/src/database/seeders/20260902000001-tax_compliance_route_access.js
@@ -3,25 +3,24 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface) {
-    const [accessRows] = await queryInterface.sequelize.query(
-      'SELECT ma_access_id FROM master_access',
-    )
-
-    for (const access of accessRows) {
-      await queryInterface.sequelize.query(
-        `INSERT INTO master_route_access
-          (mra_access_id, mra_name, mra_status)
-         SELECT ?, 'tax_compliance', 'Full Access'
-         FROM DUAL
-         WHERE NOT EXISTS (
-           SELECT 1 FROM master_route_access
-           WHERE mra_access_id = ? AND mra_name = 'tax_compliance'
-         )`,
-        {
-          replacements: [access.ma_access_id, access.ma_access_id],
-        },
-      )
-    }
+    // const [accessRows] = await queryInterface.sequelize.query(
+    //   'SELECT ma_access_id FROM master_access',
+    // )
+    // for (const access of accessRows) {
+    //   await queryInterface.sequelize.query(
+    //     `INSERT INTO master_route_access
+    //       (mra_access_id, mra_name, mra_status)
+    //      SELECT ?, 'tax_compliance', 'Full Access'
+    //      FROM DUAL
+    //      WHERE NOT EXISTS (
+    //        SELECT 1 FROM master_route_access
+    //        WHERE mra_access_id = ? AND mra_name = 'tax_compliance'
+    //      )`,
+    //     {
+    //       replacements: [access.ma_access_id, access.ma_access_id],
+    //     },
+    //   )
+    // }
   },
 
   async down(queryInterface) {
