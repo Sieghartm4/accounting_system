2b6e717d834b7b3b170ac4a7a4e7bc4931652641
Sieghartm4 <sieghartm4@gmail.com>
2026-09-17 15:23:58 +0800

    [UPDATE] bri_ledger_id alter migration commented out

--- server/src/database/migrations/create/20260811000000-add-ledger-id-to-bank-reconciliation-items.js
+++ server/src/database/migrations/create/20260811000000-add-ledger-id-to-bank-reconciliation-items.js
@@ -2,16 +2,16 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('bank_reconciliation_items', 'bri_ledger_id', {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-      references: {
-        model: 'journal_entries',
-        key: 'je_id',
-      },
-      onUpdate: 'CASCADE',
-      onDelete: 'SET NULL',
-    })
+    // await queryInterface.addColumn('bank_reconciliation_items', 'bri_ledger_id', {
+    //   type: Sequelize.INTEGER,
+    //   allowNull: true,
+    //   references: {
+    //     model: 'journal_entries',
+    //     key: 'je_id',
+    //   },
+    //   onUpdate: 'CASCADE',
+    //   onDelete: 'SET NULL',
+    // })
   },
 
   async down(queryInterface, Sequelize) {
