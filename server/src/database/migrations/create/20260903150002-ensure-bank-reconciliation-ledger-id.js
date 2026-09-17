cbd52f2858461f79b59198aed3f64186905d273a
Sieghartm4 <sieghartm4@gmail.com>
2026-09-17 15:25:52 +0800

    [UPDATE] commented out few alter migrations

--- server/src/database/migrations/create/20260903150002-ensure-bank-reconciliation-ledger-id.js
+++ server/src/database/migrations/create/20260903150002-ensure-bank-reconciliation-ledger-id.js
@@ -3,20 +3,20 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const columns = await queryInterface.describeTable('bank_reconciliation_items')
+    // const columns = await queryInterface.describeTable('bank_reconciliation_items')
 
-    if (!columns.bri_ledger_id) {
-      await queryInterface.addColumn('bank_reconciliation_items', 'bri_ledger_id', {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-        references: {
-          model: 'journal_entries',
-          key: 'je_id',
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-      })
-    }
+    // if (!columns.bri_ledger_id) {
+    //   await queryInterface.addColumn('bank_reconciliation_items', 'bri_ledger_id', {
+    //     type: Sequelize.INTEGER,
+    //     allowNull: true,
+    //     references: {
+    //       model: 'journal_entries',
+    //       key: 'je_id',
+    //     },
+    //     onUpdate: 'CASCADE',
+    //     onDelete: 'SET NULL',
+    //   })
+    // }
   },
 
   async down(queryInterface) {
